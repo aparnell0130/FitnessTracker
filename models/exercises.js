8f7f88184475226ec25fbf7dee550006f30e6d96
aparnell0130 <70460766+aparnell0130@users.noreply.github.com>
2021-02-09 00:35:16 -0800

    updated exercise model

---
+++ models/exercises.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const ExerciseSchema = new Schema({
+    type: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    distance: {
+        type: Number,
+        trim: true
+    },
+    duration: {
+        type: Number,
+        required: true,
+        trim: true
+    },
+    weight: {
+        type: Number,
+        trim: true
+    },
+    sets: {
+        type: Number,
+        trim: true
+    },
+    reps: {
+        type: Number,
+        trim: true
+    }
+});
+
+const Exercise = mongoose.model('Exercise', ExerciseSchema);
+module.exports = Exercise

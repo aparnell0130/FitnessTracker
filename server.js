36e0ad07c7f16a35787311b4298e51a61f85acd2
aparnell0130 <70460766+aparnell0130@users.noreply.github.com>
2021-02-08 23:32:35 -0800

    added listen function to server

--- server.js
+++ server.js
@@ -15,3 +15,8 @@ app.use(express.json());
 app.use(express.static('public'));
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/fitnessdb', { useNewUrlParser: true });
+
+
+app.listen(PORT, () => {
+    console.log(`App listening on http://localhost:${PORT}`)
+})

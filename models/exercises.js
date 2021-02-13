45f7b286011b68b3d6cbadf1f5df7c558a6fd2fb
aparnell0130 <70460766+aparnell0130@users.noreply.github.com>
2021-02-13 00:05:02 -0800

    fixed model now exercise id is not undefined

--- models/exercises.js
+++ models/exercises.js
@@ -2,37 +2,46 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const ExerciseSchema = new Schema({
-    type: {
-        type: String,
-        // required: true,
-        trim: true
+    day: {
+        type: Date,
+        default: function(){
+            new Date()
+        }
     },
-    name: {
-        type: String,
-        // required: true,
-        trim: true
-    },
-    distance: {
-        type: Number,
-        trim: true
-    },
-    duration: {
-        type: Number,
-        // required: true,
-        trim: true
-    },
-    weight: {
-        type: Number,
-        trim: true
-    },
-    sets: {
-        type: Number,
-        trim: true
-    },
-    reps: {
-        type: Number,
-        trim: true
-    }
+    exercises: [{
+        type: {
+            type: String,
+            trim: true,
+            required: "Enter the type of workout.",
+        },
+        name: {
+            type: String,
+            required: "What is the name of the exercise?",
+            trim: true
+        },
+        distance: {
+            type: Number,
+            trim: true
+        },
+        duration: {
+            type: Number,
+            required: "How long did you do this exercise?",
+            trim: true
+        },
+        weight: {
+            type: Number,
+            trim: true
+        },
+        sets: {
+            type: Number,
+            trim: true
+        },
+        reps: {
+            type: Number,
+            trim: true
+        }
+    }]
+    
 });
 
 const Exercise = mongoose.model('Exercise', ExerciseSchema);
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,10 +2,32 @@ const router = require('express').Router()
 const db = require('../models')
 
 router.get('/api/workouts/', (req, res) => {
+    db.Exercise.find()
+    .then(workout => {
+        res.json(workout)
+    })
+    .catch(err => {
+        res.json(err)
+    })
+})
+
+router.post('/api/workouts/', (req, res) => {
     db.Exercise.create({})
     .then(workout => {
         res.json(workout)
     })
+    .catch(err => {
+        res.json(err)
+    })
+})
+
+router.put('/api/workouts/:id', (req, res) => {
+    const workoutId = req.params.id
+    const exercise = req.body
+    db.Exercise.findByIdAndUpdate(
+        workoutId,
+        { $push: {exercises: exercise} }
+    )
 })
 
 module.exports = router

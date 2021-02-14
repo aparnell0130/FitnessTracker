eb18e66b00ed879da9b3fb85fb84d94872ebac99
aparnell0130 <70460766+aparnell0130@users.noreply.github.com>
2021-02-13 16:06:46 -0800

    used line from seed.js that looked important for server.js useFindAndModify: false

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static('public'));
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/fitnessdb', { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/fitnessdb', { useNewUrlParser: true, useFindAndModify: false });
 app.use(apiRoutes, htmlRoutes)
 
 app.listen(PORT, () => {
